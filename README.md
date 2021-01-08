377426cd35a59f2997e5c168d4bae6db0ae7efb2
eriche2016 <eriche.hust@gmail.com>
2021-01-08 16:06:56 +0800

    det for cameras

---
+++ README.md
@@ -0,0 +1,16 @@
+# YOLOV3 in ROS 
+## Detect Objects in images and visualize in RVIZ
+Codes are mainly borrowed from [project](https://github.com/vvasilo/yolov3_pytorch_ros). 
+It now supports displaying class names of bounding boxes in Chinese. 
+To use it, Please:
+```
+# git clone it 
+git clone https://github.com/eriche2016/YOLOV3_In_ROS.git
+# place run.sh and src file in catkin_ws folder  
+mkdir catkin_ws 
+mv YOLOV3_In_ROS/* catkin_ws 
+# make it 
+catkin_make 
+# then run it using run.sh 
+```
+
---
+++ run.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+# first activate openpc_det 
+echo "make sure to activate openpc_det"
+source /home/ps/hxw_projects/Hongfeng_LaneDet/ros_cv_bridge_helper/catkin_workspace/devel/setup.bash --extend 
+source devel/setup.bash 
+
+#############################################################################################
+# class names as English 
+#############################################################################################
+# original indoor bag  
+if [ 1 -eq 0 ]; then 
+    roslaunch yolov3_pytorch_ros detector.launch ros_bag_file:="/home/ps/hxw_projects/OpenPCDet_working/catkin_ws/src/hf3d_ros/bag/indoor1.bag" classes_name:="coco.names"  
+fi 
+# data collected with 32 beams and cameras 
+# just test cameras  
+if [ 1 -eq 0 ]; then 
+    roslaunch yolov3_pytorch_ros detector.launch ros_bag_file:="/media/ps/data/HF3D_Obj_Det/raw_data/2021-01-06-08-55-27.bag" classes_name:="coco.names"  
+fi
+
+
+#############################################################################################
+# class names as Manderine  
+#############################################################################################
+# original indoor bag  
+if [ 1 -eq 0 ]; then 
+    roslaunch yolov3_pytorch_ros detector.launch ros_bag_file:="/home/ps/hxw_projects/OpenPCDet_working/catkin_ws/src/hf3d_ros/bag/indoor1.bag" classes_name:="coco_cn.names" class_names_cn:=True 
+fi 
+# data collected with 32 beams and cameras 
+# just test cameras  
+if [ 1 -eq 0 ]; then 
+    roslaunch yolov3_pytorch_ros detector.launch ros_bag_file:="/media/ps/data/HF3D_Obj_Det/raw_data/2021-01-06-08-55-27.bag" classes_name:="coco_cn.names" class_names_cn:=True  
+fi
+
+if [ 1 -eq 1 ]; then 
+    # launch usb camera dirver in ROS
+    # roslaunch usb_cam usb_cam-test.launch
+    # detect it 
+    roslaunch yolov3_pytorch_ros detector_from_camera.launch classes_name:="coco_cn.names" class_names_cn:=True  
+fi 
